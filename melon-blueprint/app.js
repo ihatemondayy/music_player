a3a301c86584e8a3e61f2972ee2d95fb2286c8e8
ihatemondayy <aimeceuxquit@gmail.com>
2023-05-09 16:36:26 +0900

    add chart

--- melon-blueprint/app.js
+++ melon-blueprint/app.js
@@ -1,36 +1,27 @@
 const apiKey = '7bd879ea392a70206c883c7d76e987c4';
-const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=7bd879ea392a70206c883c7d76e987c4&format=json`;
+const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`;
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        const tracks = data.tracks.track;
-        const trackNames = tracks.map(track => track.name);
-        const playCounts = tracks.map(track => track.playcount);
-        const chartData = {
-            labels: trackNames,
-            datasets: [{
-                label: 'Play Count',
-                data: playCounts,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }]
-        };
-
-        const chartOptions = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        };
-
-        const chart = new Chart(document.getElementById('chart'), {
-            type: 'bar',
-            data: chartData,
-            options: chartOptions
-        });
+        const tracks = data.tracks.track.sort((a, b) => b.playcount - a.playcount);
+        const tableData = tracks.map((track) => {
+            return {
+              name: `${track.name} - ${track.artist.name}`,
+              playcount: parseInt(track.playcount),
+            };
+          });
+          
+          const tableBody = document.querySelector('tbody');
+          tableData.forEach((track, index) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+              <td>${index + 1}</td>
+              <td>${track.name}</td>
+              <td style="display: none;">${track.playcount}</td>
+            `;
+            tableBody.appendChild(row);
+          });
+          
+        
     });
